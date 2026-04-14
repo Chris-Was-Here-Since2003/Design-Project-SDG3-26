fef746065a571ebdd1ed2b27e2aca23c41774ed8
Crass_Crest_Chris_Cross_Crust <serenochrisclyde@gmail.com>
2026-04-14 17:34:29 +0800

    removed tabs

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,9 +4,34 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen
-        name="(tabs)"
+        name="(Stack)"
         options={{ headerShown: false }}
       />
+        <Stack.Screen 
+            name="components/FilterBar" 
+            options={{ href: null }} 
+            />
+        <Stack.Screen 
+            name="components/HospitalGrid" 
+            options={{ href: null }} 
+            />
+        <Stack.Screen 
+            name="components/HospitalCard" 
+            options={{ href: null }} 
+            />
+        <Stack.Screen 
+            name="components/PromoModal" 
+            options={{ href: null }} 
+            />
+        <Stack.Screen 
+            name="components/PromoCard" 
+            options={{ href: null }} 
+            />
+        <Stack.Screen 
+            name="components/PromoGrid" 
+            options={{ href: null }} 
+            />
+        
     </Stack>
   );
 }
